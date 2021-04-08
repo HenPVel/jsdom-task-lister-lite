72e6666440c3b911005fa9fca9c00ee381ac3d25
Henry <hvelas93@gmail.com>
2021-04-08 08:53:38 -0400

    added tasks to Ul - finished

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  const taskList = document.querySelector("#tasks") //this is the task ul
+  const formElement = document.querySelector("#create-task-form")
+  let taskElement = document.getElementById("new-task-description")
+
+  formElement.addEventListener("submit", function(event) {
+      event.preventDefault()
+
+    newTaskDescription = taskElement.value
+    const newLiToAdd = document.createElement('li')
+    newLiToAdd.innerText = newTaskDescription
+   
+    taskList.appendChild(newLiToAdd)
+    // console.log(newLiToAdd)
+
+  })
+
 });
